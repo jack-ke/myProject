41ecd75d7f93aab98ac5b4142f1bbb8fdc9f700e
ke <kmhemail@163.com>
2023-03-11 17:05:35 +0800

    对新闻详情页面进行处理

--- demo/src/component/Home/Homenews.jsx
+++ demo/src/component/Home/Homenews.jsx
@@ -1,22 +1,55 @@
-import react from "react"
+import react, { useEffect, useState } from "react"
 import NewsItem from "../NewsItem"
 import SkeletonAgain from "../SkeletonAgain"
 import "../../CSS/home.scss"
-import { Divider } from "antd-mobile"
+import { Divider, DotLoading } from "antd-mobile"
+import api from "../../api"
 
 
 const Homenews = function Homenews(props) {
+
+    const [newslist, setNewsList] = useState([])
+    useEffect(() => {
+        (async () => {
+            try {
+                api.queryNewsLatest().then(res => {
+                    let { stories, date } = res.data
+                    // 注意 这里再给里面添加一次storie是为了下面的双重循环做准备，因此需要两个数组
+                    newslist.push({ date, stories })
+                    console.log(newslist)
+                    setNewsList([...newslist])
+
+                })
+            } catch (_) { console.log("GG") }
+        })()
+    }, [])
+
     return <div className="home-news-box">
-        {/* 先放一个骨架屏 */}
-        <SkeletonAgain />
-        {/* 放新闻的详细内容 , 一个大div就是一天新闻 里面的item动态创建 */}
-        {/* 分割线组件 antd Divider*/}
-        <Divider contentPosition='left'>12月31日</Divider>
-        {/* 新闻列表 */}
-        <div className="news-list">
-            <NewsItem props={props} />
+
+        {/* 骨架屏有没有取决于有没有数据 如果有数据，就不显示骨架屏 */}
+
+        {newslist.length === 0 ? <SkeletonAgain /> :
+            <>
+                {newslist.map((item, index) => {
+                    let { stories, date, id } = item
+                    return <div key={id}>
+                        {index !== 0 ? <Divider contentPosition="left">{date}</Divider> : null}
+                        <div>
+                            {stories.map(value => {
+                                return <NewsItem info={value} myprops={props} />
+                            })}
+                        </div>
+                    </div>
+                })}
+            </>
+        }
+
+
+
+        {/* 加载更多 loading */}
+        < div className="loadmore-box" >
+            <DotLoading /> 数据加载中
         </div>
     </div>
 }
-
 export default Homenews;
--- demo/src/component/NewsItem.jsx
+++ demo/src/component/NewsItem.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import api from '../api';
+import "../CSS/home.scss";
+import temp from "../img/temp.jpg"
+import { Image } from "antd-mobile"
+import { Link } from "react-router-dom"
 
 export default function NewsItem(props) {
     const [newslist, setNewsList] = useState([])
-    // console.log(props)
+    let { myprops, info } = props
+    // console.log(myprops, info)
     useEffect(() => {
         (async () => {
             try {
@@ -21,12 +26,20 @@ export default function NewsItem(props) {
     return (
         // 新闻详细内容
         <div className='news-item-box'>
-            {newslist.map(item => {
+            {/* {newslist.map(item => {
                 let { id, title } = item
                 return <div className="content" key={id}>
                     <h4 className='title'>{title}</h4>
                 </div>
-            })}
+            })} */}
+            {/* 相信内容分为两部分 一部分是标题时间  一部分是图片 */}
+            <Link to={{ pathname: `detail/xxx` }}>
+                <div className="content">
+                    <h4 className="title">这是标题</h4>
+                    <div className='autor'>这是简介</div>
+                </div>
+                <Image src="xxx" alt="这是一张图片" lazy />
+            </Link>
         </div>
     )
 }
